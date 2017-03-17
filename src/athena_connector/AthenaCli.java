4299391f6de88386c4f52000b58a44b2cbbd3dee
nitzanav <nitzanav@gmail.com>
2017-03-17 22:19:41 +0200

    athena connector

--- src/athena_connector/AthenaCli.java
+++ src/athena_connector/AthenaCli.java
@@ -1,6 +1,9 @@
 package athena_connector;
 
-import java.nio.file.Paths;
+import java.io.FileNotFoundException;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -12,9 +15,16 @@ import org.apache.commons.cli.ParseException;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import com.amazonaws.athena.jdbc.shaded.com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
 public class AthenaCli {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
 		// create the parser
 		CommandLineParser parser = new DefaultParser();
@@ -73,19 +83,40 @@ public class AthenaCli {
 			String sql = StdInReader.read();
 
 			String s3_staging_dir_value = cmdLine.getOptionValue("s3_staging_dir");
-			if (!StringUtils.isBlank(export_to_s3_file.getValue())) {
-				String tempS3DirName = RandomStringUtils.randomAlphanumeric(36);
-				s3_staging_dir_value = Paths.get(s3_staging_dir_value, tempS3DirName).toString();
+			String export_to_s3_file_value = cmdLine.getOptionValue("export_to_s3_file");
+			if (!StringUtils.isBlank(export_to_s3_file_value)) {
+				String tempS3DirName = UUID.randomUUID().toString();
+				s3_staging_dir_value = s3_staging_dir_value + tempS3DirName + "/";
 			}
+			
 
+			String aws_credentials_provider_class_value = cmdLine.getOptionValue("aws_credentials_provider_class",
+					"com.amazonaws.auth.DefaultAWSCredentialsProviderChain");
+			String aws_credentials_provider_arguments_value = cmdLine.getOptionValue("aws_credentials_provider_arguments", "");
+			
 			AthenaConnector.execute(s3_staging_dir_value,
-					cmdLine.getOptionValue("aws_credentials_provider_class",
-							"com.amazonaws.auth.DefaultAWSCredentialsProviderChain"),
-					cmdLine.getOptionValue("aws_credentials_provider_arguments", ""),
+					aws_credentials_provider_class_value,
+					aws_credentials_provider_arguments_value,
 					cmdLine.getOptionValue("log_path", "athena_connector_jdbc.log"),
 					cmdLine.getOptionValue("log_level", "WARN"), cmdLine.getOptionValue("results_formatter"), sql);
 
-			if (!StringUtils.isBlank(export_to_s3_file.getValue())) {
+			if (!StringUtils.isBlank(export_to_s3_file_value)) {
+				AmazonS3 s3client = AmazonS3ClientBuilder.standard().withCredentials(new ProfileCredentialsProvider(aws_credentials_provider_arguments_value)).build();
+
+				
+				String s3StagingBucketName = getBucketName(s3_staging_dir_value);
+				String s3StagingFileKey = s3_staging_dir_value.substring(s3StagingBucketName.length() + 6);
+				System.out.println(s3StagingBucketName);
+				System.out.println(s3StagingFileKey);
+				String athenaResultS3Key = getAthenaResultFileS3Path(s3client, s3StagingBucketName, s3StagingFileKey); 
+
+				String exportBucketName = getBucketName(export_to_s3_file_value);
+				String exportFileKey = export_to_s3_file_value.substring(exportBucketName.length() + 6);
+				System.out.println(s3StagingBucketName);
+				System.out.println(athenaResultS3Key);
+				System.out.println(exportBucketName);
+				System.out.println(exportFileKey);
+				s3client.copyObject(s3StagingBucketName, athenaResultS3Key, exportBucketName, exportFileKey);
 			}
 
 		} catch (ParseException exp) {
@@ -95,4 +126,33 @@ public class AthenaCli {
 		}
 
 	}
+
+	private static String getBucketName(String export_to_s3_file_value) {
+		Pattern pattern = Pattern.compile("s3://(.+?)/");
+		Matcher matcher = pattern.matcher(export_to_s3_file_value);
+
+		if (matcher.find()) {
+			return matcher.group(1);
+		} else {
+		    throw new IllegalArgumentException("doesn't follow the pattern of s3://mybucket/foo/bar.");
+		}
+	}
+
+	private static String getAthenaResultFileS3Path(AmazonS3 s3client, String bucketName, String athenaResultDirPrefix) throws FileNotFoundException {
+		final ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucketName).withPrefix(athenaResultDirPrefix).withMaxKeys(2);
+		ListObjectsV2Result result;
+		do {               
+		   result = s3client.listObjectsV2(req);
+		   
+		   for (S3ObjectSummary objectSummary : 
+		       result.getObjectSummaries()) {
+			   if (objectSummary.getKey().endsWith(".csv"))
+				   return objectSummary.getKey();
+		   }
+		   req.setContinuationToken(result.getNextContinuationToken());
+		} while(result.isTruncated() == true );
+		
+		throw new FileNotFoundException("Athena result file.");
+	}
+	
 }
